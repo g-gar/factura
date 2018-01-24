4531a9bc1c82d43698f693d52bd9188535c268f3
g-gar <gabriel.garcia@intalsis.com>
2018-01-24 10:33:46 +0100

    20180124

--- app/post.php
+++ app/post.php
@@ -7,10 +7,14 @@
 
 	<h1>Welcome to the Post Page</h1>
 
-	<?php 
-		if (count($args) > 0) {
-			echo <<<EOD
-<p>Look up id number $args[0] in the database</p>
+	<?php if (count($args) > 0) { ?>
+		<p>Look up id number <?=$args[0]?> in the database</p>
+	<?php } else { ?>
+		<p>Retrieving all registers from the database</p>
+	<?php } ?>
+
+<?php
+	echo <<<EOD
 <table>
 	<thead>
 		<td>Titulo</td>
@@ -18,17 +22,25 @@
 	</thead>
 	<tbody>
 EOD;
-			$facturas = json_decode(file_get_contents('data/facturas.json'))->ALL;
-			foreach ((array)$facturas[$args[0]] as $key => $value) {
-				echo "<td>" . $value . "</td>";
+	
+	$facturas = json_decode(file_get_contents('data/facturas.json'))->ALL;
+	$ids = count($args) > 0 ? array($args[0]) : array_keys($facturas);
+
+	foreach ($ids as $i => $id) {
+		if (array_key_exists($id, array_keys($facturas))) {
+			echo "<tr>";	
+			foreach ($facturas[$id] as $j => $factura) {
+				echo "<td>" . $factura . "</td>";
 			}
+			echo "</tr>";
+		} else echo "<tr><td>Error</td><td>Error</td></tr>";
+	}
 			
-		} else echo <<<EOD
+	echo <<<EOD
 	</tbody>
 </table>
-<p>Post given is not valid</p>
 EOD;
-	?>
+?>
 		
 
 </body>
